cb8ae8b36355d595500430ab4bb73fa32f2c3d3a
Mikel Larreategi <mlarreategi@codesyntax.com>
2024-02-10 10:10:08 +0100

    use a parameter to decide which service to use

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Inspirazioa [@gailapa](https://mastodon.eus/@galaipa@mastodon.jalgi.eus) eta [@u
 
 Helburua, software jakin baten itzulpena era automatikoan egitean datza.
 
-Horretarako nik aurrez sortutako [YogiTea mezu-txiokatzailea](https://github.com/erral/yogitea-txiokatzailea/) proiektuan erabilitako scriptak berrerabili ditut [elia.eus](https://elia.eus), [batua.eus](https://batua.eus) eta [itzuli.eus](https://itzuli.eus) web zerbitzuak ingelesezko mezuak euskarara itzultzeko.
+Horretarako nik aurrez sortutako [YogiTea mezu-txiokatzailea](https://github.com/erral/yogitea-txiokatzailea/) proiektuan erabilitako scriptak berrerabili ditut [elia.eus](https://elia.eus), [batua.eus](https://batua.eus) eta [itzuli](https://www.euskadi.eus/itzuli/) web zerbitzuak ingelesezko mezuak euskarara itzultzeko.
 
 ## Nola erabili
 
@@ -18,10 +18,10 @@ pip install -r requirements.txt
 
 2. Ondoren itzuli behar den fitxategia lortu.
 
-3. Azkenik scripta exekutatu:
+3. Azkenik scripta exekutatu fitxategiaren izena eta erabili beharreko zerbitzua parametro gisa pasatuz. Azken honetarako 3 aukera dituzu, batua, itzuli edo elia:
 
 ```bash
-python itzuli.py fitxategia.po
+python itzuli.py fitxategia.po itzuli
 ```
 
 Itzulpena egiteko erabiltzen den zerbitzu bakoitzeko fitxategi bat sortuko du: `eliaeus.po`, `batuaeus.po` eta `itzulieus.po`.
--- itzuli.py
+++ itzuli.py
@@ -3,7 +3,7 @@ import argparse
 import polib
 import requests
 import tqdm
-
+import enum
 
 def translate_text_itzuli_eus(text: str) -> str:
     """itzuli.eus webgunea erabiliz testu bat ingelesetik euskarara itzuli"""
@@ -87,63 +87,76 @@ def translate_text_batua_eus(text:str) -> str:
     return ""
 
 
-ALL_TRANSLATION_FUNCTIONS = [
-    {
-        "name": "itzuli.eus",
-        "action": translate_text_itzuli_eus,
-    },
-    {
+class Itzultzailea(enum.Enum):
+    """ Itzultzaile posibleak gordetzeko Enum objektua"""
+    elia = 'elia'
+    itzuli = 'itzuli'
+    batua = 'batua'
+
+    def __str__(self):
+        return self.value
+
+
+ALL_TRANSLATION_FUNCTIONS = {
+    Itzultzailea.batua: {
         "name": "batua.eus",
         "action": translate_text_batua_eus,
     },
-    {
+    Itzultzailea.elia: {
         "name": "elia.eus",
         "action": translate_text_elia_eus,
     },
-]
+    Itzultzailea.itzuli:{
+        "name": "itzuli.eus",
+        "action": translate_text_itzuli_eus,
+    },
+}
 
-translation_functions = ALL_TRANSLATION_FUNCTIONS[0:1]
 
-def main(filepath:str) -> None:
+def main(filepath:str, itzultzailea: Itzultzailea) -> None:
     """ Open the PO file and get all msgids in order to translate them
     """
     pofile = polib.pofile(filepath)
 
-    for translation_function in translation_functions:
-        new_contents = []
-        name = translation_function.get('name')
-        translate = translation_function.get("action")
+    translation_function = ALL_TRANSLATION_FUNCTIONS.get(itzultzailea)
 
-        print(f'Erabiltzen ari garen itzultzailea: {name}')
+    new_contents = []
+    name = translation_function.get('name')
+    translate = translation_function.get("action")
 
-        print(f'Itzuli beharreko elementu kopurua: {len(pofile)}')
+    print(f'Erabiltzen ari garen itzultzailea: {name}')
 
-        for entry in tqdm.tqdm(pofile):
+    print(f'Itzuli beharreko elementu kopurua: {len(pofile)}')
+
+    for entry in tqdm.tqdm(pofile):
+        try:
             translation = translate(entry.msgid)
             new_contents.append([entry.msgid, translation, entry.occurrences])
+        except Exception as e:
+            print('Huts egin du mezua itzultzean')
 
-        translated_pofile = polib.POFile()
-        translated_pofile.metadata = {
-            'Project-Id-Version': '1.0',
-            'Report-Msgid-Bugs-To': 'you@example.com',
-            'POT-Creation-Date': '2007-10-18 14:00+0100',
-            'PO-Revision-Date': '2007-10-18 14:00+0100',
-            'Last-Translator': name,
-            'Language-Team': 'Euskara <eu@li.org>',
-            'MIME-Version': '1.0',
-            'Content-Type': 'text/plain; charset=utf-8',
-            'Content-Transfer-Encoding': '8bit',
-        }
-        print('Fitxategia sortzen')
-        for msgid, msgstr, ocurrences in new_contents:
-            entry = polib.POEntry(
-                msgid=msgid,
-                msgstr=msgstr,
-                ocurrences=ocurrences
-            )
-            translated_pofile.append(entry)
-        translated_pofile.save(f'{name}.po')
-        print('Eginda!')
+    translated_pofile = polib.POFile()
+    translated_pofile.metadata = {
+        'Project-Id-Version': '1.0',
+        'Report-Msgid-Bugs-To': 'you@example.com',
+        'POT-Creation-Date': '2007-10-18 14:00+0100',
+        'PO-Revision-Date': '2007-10-18 14:00+0100',
+        'Last-Translator': name,
+        'Language-Team': 'Euskara <eu@li.org>',
+        'MIME-Version': '1.0',
+        'Content-Type': 'text/plain; charset=utf-8',
+        'Content-Transfer-Encoding': '8bit',
+    }
+    print('Fitxategia sortzen')
+    for msgid, msgstr, ocurrences in new_contents:
+        entry = polib.POEntry(
+            msgid=msgid,
+            msgstr=msgstr,
+            ocurrences=ocurrences
+        )
+        translated_pofile.append(entry)
+    translated_pofile.save(f'{name}.po')
+    print('Eginda!')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -151,7 +164,13 @@ if __name__ == "__main__":
         description="PO fitxategia euskarazko itzultzaile "
                     "automatikoak erabiliz euskarara itzultzeko tresna"
     )
-    parser.add_argument('filename', help='Itzuli beharreko po fitxategia')
+    parser.add_argument('fitxategia', help='Itzuli beharreko po fitxategia')
+    parser.add_argument(
+        'itzultzailea',
+        help='Aukeratu zein itzultzaile erabili',
+        type=Itzultzailea,
+        choices=(Itzultzailea.batua, Itzultzailea.elia, Itzultzailea.itzuli)
+    )
     args = parser.parse_args()
 
-    main(args.filename)
+    main(args.fitxategia, args.itzultzailea)
